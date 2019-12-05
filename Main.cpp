54075055c752b975c9a0e997c2648d9c474d8a1d
Felipe Augusto <augustofelipe147@gmail.com>
2019-12-05 20:14:12 -0300

    Adicionou alterações na main

--- Main.cpp
+++ Main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int main(){
     setlocale(LC_ALL,"");
     Exame *p1=new Exame();
+    p1->Cadastrar();
     p1->Laudo();
     cout<<"Resultado:"<<endl<<p1->getResultado()<<endl;
     return 0;
